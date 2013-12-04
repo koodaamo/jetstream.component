a3a90f35b7de818b71bdd63d79d54fbf83f2804b
Petri Savolainen <petri.savolainen@iki.fi>
2013-12-04 08:58:44 +0200

    fix component type alterative cookiecutter question, add klass name question; fix implementation template accordingly

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name.split('.')[0]}}/{{cookiecutter.repo_name.split('.')[1]}}/{{cookiecutter.repo_name.split('.')[1]}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name.split('.')[0]}}/{{cookiecutter.repo_name.split('.')[1]}}/{{cookiecutter.repo_name.split('.')[1]}}.py
@@ -1,22 +1,20 @@
 """
-Implement a Jetstream {{component_type}}Component
+Implement a Jetstream {{cookiecutter.component_type}} component
 """
 
+from jetstream.base import {{cookiecutter.component_type}}Component
 
-from jetstream.base import {{component_type}}Component
-
-# Here's the component boilerplate. It inherits from a Jetstream
-# base {{component_type}} component, so is already runnable:
+# Here's your component boilerplate. It inherits from a Jetstream
+# base {{cookiecutter.component_type}} component, so is already runnable:
 # Your can add it to your Jetstream configuration and try it out!
 
-class {{component_name}}{{component_type}}Component({{component_type}}Component):
-   "a {{component_type}}Component class"
+class {{cookiecutter.component_klass}}({{cookiecutter.component_type}}Component):
+   "a {{cookiecutter.component_type}}Component class"
 
 
-# Here's a simple example of a dummy Input component that can also
-# act as a pass-through component if it's given a stream
+# Here's a simple example of a dummy Input component
 
-fro jetstream.base import InputComponent
+from jetstream.base import InputComponent
 
 class InputComponent(InputComponent):
    "an example Input component"
@@ -28,10 +26,8 @@ class InputComponent(InputComponent):
    def __init__(self, stream, config):
       "configure the component"
       # use a static dummy data source
-      self._stream = stream or ({"key1":"value1"}, {"key2":"value"})
+      self._stream = ({"key1":"value1"}, {"key2":"value"})
       self._config = config
-
-   # Implementing __iter__ is almost always necessary
 
    def __iter__(self):
       "produce the result"
